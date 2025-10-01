480f8ebdff0df21f4c75d6381cca9be43a8424e2
Hugoo21 <hugoloreau21@gmail.com>
2025-10-01 12:51:38 +0200

    added a function to filter the data by dates

--- src/main.py
+++ src/main.py
@@ -1,15 +1,23 @@
 import pandas 
+import datetime
 import matplotlib.pyplot as plt
-import csv
+
+start_date = pandas.Timestamp('2022-01-01')
+end_date = pandas.Timestamp('2022-12-30')
 
 def open_file(input_file):
+    # open and read the csv file created in generatedata.py
     try:
         with open(input_file, 'r') as file:
-            reader = csv.reader(file)
-            for row in reader:
-                print(row)
+            df = pandas.read_csv(file, parse_dates=['date'])
+            return df
     except FileNotFoundError:
         print("Error : The file", input_file, "was not found.")
 
+def filter_by_dates(start_date, end_date, df):
+    df['date'] = pandas.to_datetime(df['date']) # converts the dates to a timestamp format
+    filtered_by_dates_df = df[(df["date"] >= start_date) & (df["date"] <= end_date)] # we check if the dates of the date column belong to the given interval
+    return filtered_by_dates_df
 
-open_file('climate_data.csv')
+df = open_file('climate_data.csv')
+filtered_by_dates_df = filter_by_dates(start_date, end_date, df)
